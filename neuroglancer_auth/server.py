db39b638fdb9479abe53a380a364bdbeeb1a7f7f
Chris Jordan <jordanchriss@gmail.com>
2025-04-30 16:10:23 -0400

    Bump version: 2.29.0 → 2.30.0

--- neuroglancer_auth/server.py
+++ neuroglancer_auth/server.py
@@ -36,7 +36,7 @@ from .model.user_custom_name import UserCustomName
 from .model.user_group import UserGroup
 from .model.user_tos import UserTos
 
-__version__ = "2.29.0"
+__version__ = "2.30.0"
 
 
 def permissionLookUp(token):
